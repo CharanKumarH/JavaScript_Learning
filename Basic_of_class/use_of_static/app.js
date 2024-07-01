6d0ba60fd74305766a5871154c370422b4a592f4
Charan Kumar H <charankumarh4@gmail.com>
2024-07-02 00:23:29 +0530

    sorting array using static methods use of static inside class

--- Basic_of_class/use_of_static/app.js
+++ Basic_of_class/use_of_static/app.js
@@ -1,41 +1,34 @@
-// /**
-//  * static method
-//  * static properties
-//  *
-//  */
+/**
+ * static method
+ * static properties
+ *
+ */
 
-// class Children {
-//   constructor(name, age) {
-//     this.name = name;
-//     this.age = age;
-//   }
-//   static getChildren() {
-//     return new Children("John", 10);
-//   }
-// }
+class Children {
+  static id = 1;
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    this.id = Children.id++;
+  }
+  static SortByAge(a, b) {
+    return a.age - b.age;
+  }
+  static SortByName(a, b) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  }
+}
 
-// const child1 = new Children("e", 11);
-// const child2 = new Children("d", 18);
-// const child3 = new Children("c", 6);
-// const child4 = new Children("b", 15);
-// const child5 = new Children("a", 15);
+const child1 = new Children("e", 11);
+const child2 = new Children("d", 18);
+const child3 = new Children("c", 6);
+let child4 = new Children("b", 15);
+let child5 = new Children("a", 15);
+child4 = null;
 
-// let arr = [child1, child2, child3, child4, child5];
+let arr = [child1, child2, child3, child5];
 
-// // console.log(arr.sort((a, b) => a.age - b.age));
-// // console.log(arr.sort((a, b) => b.age - a.age));
-
-// // console.log(arr.sort((a, b) => a.name - b.name));
-// console.log(
-//   arr.sort((a, b) => {
-//     console.log(a, b);
-//     if (a.name < b.name) {
-//       return -1;
-//     }
-//     if (a.name > b.name) {
-//       return 1;
-//     }
-
-//     return 0;
-//   })
-// );
+console.log([...arr].sort(Children.SortByAge));
+console.log([...arr].sort(Children.SortByName));
