e3b2f757d631049ae0c02b7181122f465c1bdcd6
Charan Kumar H <charankumarh4@gmail.com>
2024-07-01 15:35:11 +0530

    "ascdfgf"

---
+++ Basic_of_class/as_a_function/app.js
@@ -0,0 +1,10 @@
+function User(name, age) {
+  var name = name;
+  var age = age;
+  console.log(name, age);
+}
+// const User1 = User();
+// const User2 = User();
+const User3 = User("John", 30);
+// console.log(User1, User2, User3);
+console.log(User.age);
